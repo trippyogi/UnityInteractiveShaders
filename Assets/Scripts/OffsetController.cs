bd40732e171e5874fd06fa376a01739f4fd2acd8
Jeremy <peace@trippyogi.com>
2020-05-29 23:32:20 -0700

    Rename OffsetController

--- Assets/Scripts/OffsetController.cs
+++ Assets/Scripts/OffsetController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinesController : MonoBehaviour
+public class OffsetController : MonoBehaviour
 {
     private float _audioLevelReceiver;
     private float _offset;
